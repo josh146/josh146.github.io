18852fbd63085439c12f61ac590723d1d7f931bb
Joshua Izaac <josh.izaac@uwa.edu.au>
2014-03-22 16:09:34 +0800

    Removed iScroll

--- pelican-themes/BT3-Flat/static/js/parallax.js
+++ pelican-themes/BT3-Flat/static/js/parallax.js
@@ -1,48 +1,38 @@
-(function(){
-  //set up iScroll and stellar
-  var ua = navigator.userAgent,
-    isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
+var ua = navigator.userAgent,
+  isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
 
-  var iOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
+var iOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
+
+function isIE() {
+  return ((navigator.appName == 'Microsoft Internet Explorer') || ((navigator.appName == 'Netscape') && (new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null)));
+}
+
+function createParallax(imagetype,speedVal) {
+  $(imagetype).parallax({ speed: speedVal, axis: 'y' });
+  $('.image-caption').parallaxDiv({ speed: -speedVal, axis: 'y' });
+  $(imagetype).parallaxUpdate({ speed: speedVal, axis: 'y' });
+}
+
+(function(){
 
   if (isMobileWebkit) {
     // $('html').addClass('mobile');
   }
 
   if (iOS) {
-    // $("html, body").css("overflow", "scroll");
-    // $("html, body").css("-webkit-overflow-scrolling", "touch");
     $("#ipadBG").css("display", "block");
     $(".fullscreen-img").css("display", "none");
   };
 
   $(function(){
-    // var iScrollInstance;
 
-    if (isMobileWebkit) {
-      // iScrollInstance = new iScroll('wrapper');
+    if (!isMobileWebkit) {
 
-      // $('#scroller').stellar({
-      //   scrollProperty: 'transform',
-      //   positionProperty: 'transform',
-      //   horizontalScrolling: false,
-      //   verticalOffset: 50
-      // });
-    } else {
-      if ( $(window).width() > 767) {
-        $.stellar({
-          horizontalScrolling: false,
-          verticalOffset: 400
-        });
-      } else {
-        $.stellar({
-          horizontalScrolling: false,
-          verticalOffset: 50
-        });        
+        if (!isIE()) {
+          $("body").niceScroll();
+        }
+
       }
-
-      $("body").niceScroll();
-    }
   });
 
   // homemade parallax
@@ -72,6 +62,34 @@
                   yPos = isX ? origY : ( dist() * options.speed ) + 'px';
               $this.css( 'background-position', xPos + ' calc(' + origY + ' + ' + yPos +')' );
           });
+      });
+  };
+
+  // homemade parallax
+  $.fn.parallaxUpdate = function ( userSettings ) {
+      var options = $.extend( {}, $.fn.parallax.defaults, userSettings );
+
+      return this.each(function () {
+          var $this = $(this),
+              isX = options.axis === 'x',
+              origPos = ( $this.css( 'background-position' ) || '' ).split(' '),
+              origX = $this.css( 'background-position-x' ) || origPos[ 0 ],
+              origY = $this.css( 'background-position-y' ) || origPos[ 1 ],
+              dist = function () {
+                  return -$( window )[ isX ? 'scrollLeft' : 'scrollTop' ]();
+              };
+          $this
+              // .css( 'background-attachment', 'fixed' )
+              .addClass( 'inview' );
+
+          $this.bind('inview', function ( e, visible ) {
+              $this[ visible ? 'addClass' : 'removeClass' ]( 'inview' );
+          });
+
+          if ( !$this.hasClass( 'inview' )) { return; }
+          var xPos = isX ? ( dist() * options.speed ) + 'px' : origX,
+              yPos = isX ? origY : ( dist() * options.speed ) + 'px';
+          $this.css( 'background-position', xPos + ' calc(' + origY + ' + ' + yPos +')' );
       });
   };
 
